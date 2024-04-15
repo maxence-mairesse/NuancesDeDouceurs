c064cc70967568adf24f4dbd4f216929dc944ef3
Moscato1 <mairessemax@live.fr>
2024-04-15 14:22:15 +0200

    add branch dev

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Nuances de douceurs</h1>
         <img src={logo} className="App-logo" alt="logo" />
 
         <p>
