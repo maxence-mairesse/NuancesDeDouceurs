c09b15f0c9cb8e386d915f343a22c815ae71c992
Moscato1 <mairessemax@live.fr>
2024-04-15 14:17:35 +0200

    vercel speedInsights

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,27 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import logo from '../../assets/logo.svg';
 import './App.scss';
+// eslint-disable-next-line import/order
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>
+          Edit <code>src/components/App/App.jsx</code> and save to reload.
+        </p>
+
+        <a
+          className="App-link"
+          href="https://react.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
       <SpeedInsights />
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import App from './components/App/App';
 
 import './styles/index.scss';
