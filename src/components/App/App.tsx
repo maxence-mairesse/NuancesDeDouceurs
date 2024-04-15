84fb522e6bb3b8ed357e2d157636940dd1a1605f
Moscato1 <mairessemax@live.fr>
2024-04-15 14:14:01 +0200

    vercel speedInsights

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,11 @@
-import logo from '../../assets/logo.svg';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          Edit <code>src/components/App/App.jsx</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://react.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SpeedInsights />
     </div>
   );
 }
