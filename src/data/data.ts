03c285044f30ec78324dd865065a774b4eb52976
Moscato1 <mairessemax@live.fr>
2024-04-16 14:53:57 +0200

    chemin img

--- src/data/data.ts
+++ src/data/data.ts
@@ -3,7 +3,7 @@ export const data = [
     id: 1,
     title: 'Une esthéticienne qui prend soin de vous',
     description: 'Description',
-    image: '../../assets/me-decouvrir.jpeg',
+    image: '/../src/assets/img/me-decouvrir.jpeg',
     links: {
       title: 'Me découvrir',
       link: '/galerie',
@@ -13,7 +13,7 @@ export const data = [
     id: 2,
     title: 'Envie de detétente et de bien être ?',
     description: 'Description',
-    image: '../../assets/masque-boue.jpeg',
+    image: '/../src/assets/img/masque-boue.jpeg',
     links: {
       title: 'Prendre rendez-vous',
       link: '/rendez-vous',
@@ -23,7 +23,7 @@ export const data = [
     id: 3,
     title: 'Découvrez les soins',
     description: 'Description',
-    image: '../../assets/palette-maquillage.jpeg',
+    image: '/../src/assets/img/palette-maquillage.jpeg',
     links: {
       title: 'La carte des soins',
       link: '/carte-soins',
