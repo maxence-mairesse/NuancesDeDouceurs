f8570e4a26bf15a8da47103ca8e41094a04ce9df
Moscato1 <mairessemax@live.fr>
2024-04-17 18:38:03 +0200

    css page carte

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,25 @@
 import '../../../public/style/index.css';
 import './Home.scss';
 import Carrousel from '../Carrousel/Carrousel';
+import Infos from '../Infos/Infos';
 
 const Home = () => {
   return (
     <div className="Home">
       <div className="Carrousel">
         <Carrousel />
+      </div>
+      <div className="infos">
+        <h3>
+          <span>
+            <i className="fa-solid fa-triangle-exclamation" />
+          </span>{' '}
+          Annonces{' '}
+          <span>
+            <i className="fa-solid fa-triangle-exclamation" />
+          </span>
+        </h3>
+        <Infos />
       </div>
     </div>
   );
---
+++ src/components/Infos/Infos.tsx
@@ -0,0 +1,15 @@
+import './infos.scss';
+
+const Infos = () => {
+  return (
+    <div className="info">
+      <h3>Nouvelles prestations !</h3>
+      <ul>
+        <li>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Infos;
